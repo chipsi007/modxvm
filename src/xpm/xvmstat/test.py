9a89dba801aca7c23e6db1a116712e3d0a6b6cb5
seriych <seriych@gmail.com>
2014-12-22 15:25:44 +0400

    tune default config

---
+++ src/xpm/xvmstat/test.py
@@ -0,0 +1,20 @@
+""" XVM (c) www.modxvm.com 2013-2014 """
+
+#############################
+# Command
+
+def runTest(args):
+    if args is None:
+        return
+    cmd = args[0]
+
+    if cmd == 'battleResults':
+        _showBattleResults(args[1])
+
+###
+
+# BattleResults
+
+def _showBattleResults(arenaUniqueID):
+    from gui.shared import event_dispatcher as shared_events
+    shared_events.showBattleResults(arenaUniqueID)
---
+++ src/xvm/wg/app/net/wg/data/constants/ValObject.as
@@ -0,0 +1,13 @@
+package net.wg.data.constants
+{
+    public class ValObject extends Object
+    {
+        
+        public function ValObject()
+        {
+            super();
+        }
+        
+        public static var EXTRA_MODULE_INFO:String = "extraModuleInfo";
+    }
+}
