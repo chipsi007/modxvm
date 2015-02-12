ac1601ac3f02e1f647eac5f26b156d9ab88264de
Alexei Nikitin <mr-a1@yandex.ru>
2015-02-12 21:34:36 +0300

    fix

---
+++ release/l10n/ru.xc
@@ -0,0 +1,188 @@
+﻿/**
+ * Russian translation file.
+ */
+{
+  "locale": {
+    // Common
+    "Warning": "Предупреждение",
+    "Error": "Ошибка",
+    "Information": "Информация",
+    "OK": "OK",
+    "Cancel": "Отмена",
+    "Save": "Сохранить",
+    "Remove": "Удалить",
+    "Yes": "Да",
+    "No": "Нет",
+
+    // Ping
+    "Initialization": "Инициализация",
+
+    // Win chance
+    "Chance error": "Ошибка расчета шансов",
+    "Chance to win": "Шансы на победу",
+    "Team strength": "Силы команд",
+    //"global": "общий",
+    //"per-vehicle": "по технике",
+    "chanceLive": "Для живых",
+    "chanceBattleTier": "Уровень боя",
+
+    // Hitlog
+    "shot": "атака",
+    "fire": "пожар",
+    "ramming": "таран",
+    "world_collision": "падение",
+    "death_zone": "death zone",
+    "drowning": "drowning",
+
+    "Hits": "Пробитий",
+    "Total": "Всего",
+    "Last": "Последний",
+
+    // Hp Left
+    "hpLeftTitle": "Осталось HP:",
+
+    // Capture
+    "enemyBaseCapture": "Захват базы{0} союзниками!",
+    "enemyBaseCaptured": "База{0} захвачена союзниками!",
+    "allyBaseCapture": "Захват базы{0} врагами!",
+    "allyBaseCaptured": "База{0} захвачена врагами!",
+    "Timeleft": "Осталось",
+    "Capturers": "Захватчиков",
+
+    // BattleResults
+    "Hit percent": "Процент попаданий",
+    "Damage (assisted / own)": "Урон (с вашей помощью / свой)",
+    "BR_xpCrew": "экипажу",
+
+    // TeamRenderers
+    "Friend": "Друг",
+    "Ignored": "Игнор",
+    "unknown": "неизвестно",
+    "Fights": "Боёв",
+    "Wins": "Побед",
+    "Data was updated at": "Данные были обновлены",
+    "Load statistics": "Загрузить статистику",
+
+    // UserInfo
+    "General stats": "Общая статистика",
+    "Summary": "Общие результаты",
+    "Avg level": "Средний уровень",
+    "WN6": "WN6",
+    "WN8": "WN8",
+    "EFF": "РЭ",
+    "WGR": "ЛРИ",
+    "updated": "обновлено",
+    " to ": " до ",
+    "avg": "ср.",
+    "top": "топ",
+    "draws": "ничьих",
+    "Maximum damage": "Максимальный урон",
+    "Specific damage (Avg dmg / HP)": "Уд. урон (ср. урон / прочность)",
+    "Capture points": "Очки захвата",
+    "Defence points": "Очки защиты",
+    "Filter": "Фильтр",
+    "Extra data (WoT 0.8.8+)": "Доп. данные (WoT 0.8.8+)",
+    "Average battle time": "Среднее время жизни в бою",
+    "Average battle time per day": "Среднее время игры в день",
+    "Battles after 0.8.8": "Боев после 0.8.8",
+    "Average experience": "Средний опыт",
+    "Average experience without premium": "Средний опыт без премиума",
+    "Average distance driven per battle": "В среднем пройдено км за бой",
+    "Average woodcuts per battle": "В среднем повалено деревьев за бой",
+    "Average damage assisted": "Средний урон с вашей помощью",
+    "    by tracking": "    после сбития гусеницы",
+    "    by spotting": "    по разведданным",
+    "Average HE shells fired (splash)": "Средний урон фугасами (сплэш)",
+    "Average HE shells received (splash)": "Средний полученный урон фугасами",
+    "Average penetrations per battle": "В среднем пробитий за бой",
+    "Average hits received": "В среднем получено попаданий",
+    "Average penetrations received": "В среднем получено пробитий",
+    "Average ricochets received": "В среднем получено рикошетов",
+
+    // Crew
+    "PutOwnCrew": "Родной экипаж",
+    "PutBestCrew": "Лучший экипаж",
+    "PutClassCrew": "Экипаж того же класса",
+    "PutPreviousCrew": "Предыдущий экипаж",
+    "DropAllCrew": "Высадить весь экипаж",
+
+    // Vehicle Params
+    "gun_reload_time/actual": "Расчетное время перезарядки орудия",
+    "view_range/base": "базовый",
+    "view_range/actual": "расчетный",
+    "view_range/stereoscope": "со стереотрубой",
+    "radio_range/base": "базовая",
+    "radio_range/actual": "расчетная",
+    "(sec)": "(сек)",
+    "(m)": "(м)",
+
+    // Squad
+    "Squad battle tiers": "Уровень боев взвода",
+    "Vehicle": "Танк",
+    "Battle tiers": "Уровень боёв",
+    "Type": "Тип",
+    "Nation": "Нация",
+    "ussr": "СССР",
+    "germany": "Германия",
+    "usa": "США",
+    "france": "Франция",
+    "uk": "Великобритания",
+    "china": "Китай",
+    "japan": "Япония",
+    "HT": "ТТ",
+    "MT": "СТ",
+    "LT": "ЛТ",
+    "TD": "ПТ",
+    "SPG": "САУ",
+
+    // VehicleMarkersManager
+    "blownUp": "Взрыв БК!",
+
+    // Check version
+    // XVM 5.3.4 (4321)
+    "ver/currentVersion": "XVM {0} ({1})",
+    "ver/newVersion": "Доступно обновление:<tab/><a href='#XVM_SITE_DL#'><font color='#00FF00'>v{0}</font></a>\n{1}",
+    "websock/not_connected": "<font color='#FFFF00'>нет подключения к серверу XVM</font>",
+
+    // Token
+    "token/services_unavailable": "Сетевые сервисы недоступны.&nbsp;&nbsp;<a href='#XVM_SITE_UNAVAILABLE#'><font size='11'>подробнее</font></a>",
+    "token/services_inactive": "Сетевые сервисы неактивны.&nbsp;&nbsp;<a href='#XVM_SITE_INACTIVE#'><font size='11'>подробнее</font></a>",
+    "token/blocked": "Статус: <font color='#FF0000'>Заблокирован</font>&nbsp;&nbsp;<a href='#XVM_SITE_BLOCKED#'><font size='11'>подробнее</font></a>",
+    "token/active": "Статус:<tab/><font color='#00FF00'>Активен</font>",
+    "token/time_left": "Осталось:<tab/><font color='#EEEEEE'>{0}д. {1}ч. {2}м.</font>",
+    "token/time_left_warn": "Осталось:<tab/><font color='#EEEE00'>{0}д. {1}ч. {2}м.</font>",
+    "token/cnt": "Количество запросов:<tab/><font color='#EEEEEE'>{0}</font>",
+    "token/unknown_status": "Неизвестный статус",
+
+    // Carousel
+    "NonElite": "Не элитный",
+    "Premium": "Премиум",
+    "Normal": "Обычный",
+    "MultiXP": "Мультиопыт",
+    "NoMaster": "Нет мастера",
+    "CompleteCrew": "Полный экипаж",
+
+    // Comments
+    "Error loading comments": "Ошибка загрузки комментариев",
+    "Error saving comments": "Ошибка сохранения комментариев",
+    "Comments disabled": "Комментарии отключены",
+    "Edit data": "Изменить данные",
+    "Nick": "Имя",
+    "Group": "Группа",
+    "Comment": "Комментарий",
+
+    // Vehicle status
+    "Destroyed": "Уничтожен",
+    "No data": "Нет данных",
+    "Not ready": "Не готов",
+
+    // Quests
+    "Hide with honors": "Скрыть с отличием",
+    "Hide not available": "Скрыть недоступные",
+
+    // Config loading
+    "XVM config reloaded": "Конфиг XVM перезагружен",
+    "Config file xvm.xc was not found, using the built-in config": "Файл конфигурации xvm.xc не найден, используем встроенную конфигурацию",
+    "Error loading XVM config": "Ошибка загрузки конфига XVM"
+  }
+}
---
+++ src/xpm/xvm_main/token.py
@@ -0,0 +1,274 @@
+""" XVM (c) www.modxvm.com 2013-2015 """
+
+# PUBLIC
+
+versionChecked = False
+
+def checkVersion():
+    _checkVersion()
+
+def initializeXvmToken():
+    _initializeXvmToken()
+
+def getXvmActiveTokenData():
+    return _getXvmActiveTokenData()
+
+def getXvmMessageHeader():
+    return _getXvmMessageHeader()
+
+def getToken():
+    _getXvmActiveTokenData()
+    global _token
+    return _token
+
+def clearToken(value=None):
+    global _token
+    _token = value
+    global networkServicesSettings
+    networkServicesSettings = _makeNetworkServicesSettings(None)
+
+def getClanInfo(clanAbbrev):
+    global _clansInfo
+    return _clansInfo.get(clanAbbrev, None) if _clansInfo else None
+
+# PRIVATE
+
+from pprint import pprint
+from random import randint
+import traceback
+import datetime
+import time
+import urllib
+
+import simplejson
+
+import BigWorld
+from gui import SystemMessages
+
+from xfw import *
+
+import config
+from constants import *
+from logger import *
+from loadurl import loadUrl
+import userprefs
+import utils
+from websock import g_websock
+
+_clansInfo = None
+_verInfo = None
+_tdataPrev = None
+_token = None
+
+def _makeNetworkServicesSettings(tdata):
+    svc = {} if tdata is None else tdata.get('services', {})
+    active = tdata is not None and tdata.get('token', None) is not None
+    return {
+        'servicesActive': active,
+        'statBattle': svc.get('statBattle', active),
+        'statAwards': svc.get('statAwards', active),
+        'statCompany': svc.get('statCompany', active),
+        'comments': svc.get('comments', active),
+        'chance': svc.get('chance', False),
+        'chanceLive': svc.get('chanceLive', False),
+        'topClansCount': svc.get('topClansCount', 100),
+    }
+
+networkServicesSettings = _makeNetworkServicesSettings(None)
+
+def _getXvmActiveTokenData():
+    playerId = getCurrentPlayerId()
+    if playerId is None:
+        return None
+
+    tdata = userprefs.get('tokens.{0}'.format(playerId))
+    if tdata is None:
+        # fallback to the last player id if replay is running
+        if isReplay():
+            playerId = userprefs.get('tokens.lastPlayerId')
+            if playerId is None:
+                return None
+            tdata = userprefs.get('tokens.{0}'.format(playerId))
+
+    if tdata is not None and not 'token' in tdata:
+        tdata = None
+
+    if tdata is not None:
+        global _token
+        _token = tdata.get('token', '').encode('ascii')
+        global networkServicesSettings
+        networkServicesSettings = _makeNetworkServicesSettings(tdata)
+    return tdata
+
+def _checkVersion():
+    playerId = getCurrentPlayerId()
+    # fallback to the last player id if replay is running
+    if playerId is None and isReplay():
+        playerId = userprefs.get('tokens.lastPlayerId')
+    if playerId is None:
+        return
+
+    global versionChecked
+    versionChecked = True
+
+    try:
+        req = "checkVersion/%d" % playerId
+        server = XVM_SERVERS[randint(0, len(XVM_SERVERS) - 1)]
+        (response, duration, errStr) = loadUrl(server, req)
+
+        #response =
+        """ {
+              "topClans":{"MGRD":{"rank":"747","cid":"156781"},...},
+              "persistClans":{"WG-A":{"rank":"0","cid":"17996"},...},
+              "info":{"RU": {"message":"www.modxvm.com","ver":"5.2.1-test2"},...}
+            }"""
+
+        global _clansInfo, _verInfo
+        _clansInfo = None
+        _verInfo = None
+        if not response:
+            #err('Empty response or parsing error')
+            pass
+        else:
+            try:
+                if response is not None:
+                    response = response.strip()
+                    if response not in ('', '[]'):
+                        data = simplejson.loads(response)
+                        if data is not None:
+                            _clansInfo = _processClansInfo(data)
+                            _verInfo = data.get('info', None)
+            except Exception, ex:
+                err('  Bad answer: ' + response)
+                err(traceback.format_exc())
+                _clansInfo = None
+                _verInfo = None
+    except Exception, ex:
+        err(traceback.format_exc())
+
+def _initializeXvmToken():
+    global _tdataPrev
+    clearToken()
+
+    playerId = getCurrentPlayerId()
+    if playerId is None:
+        return
+
+    tdataActive = _getXvmActiveTokenData()
+    (tdata, errStr) = _checkToken(playerId, None if tdataActive is None else tdataActive['token'])
+    if tdata is None:
+        tdata = _tdataPrev
+
+    type = SystemMessages.SM_TYPE.Warning
+    msg = _getXvmMessageHeader()
+    if tdata is None:
+        msg += '{{l10n:token/services_unavailable}}\n\n%s' % utils.hide_guid(errStr)
+    elif tdata['status'] == 'badToken' or tdata['status'] == 'inactive':
+        msg += '{{l10n:token/services_inactive}}'
+    elif tdata['status'] == 'blocked':
+        msg += '{{l10n:token/blocked}}'
+    elif tdata['status'] == 'active':
+        type = SystemMessages.SM_TYPE.GameGreeting
+        msg += '{{l10n:token/active}}\n'
+        s = time.time()
+        e = tdata['expires_at']/1000
+        days_left = int((e - s) / 86400)
+        hours_left = int((e - s) / 3600) % 24
+        mins_left = int((e - s) / 60) % 60
+        token_name = 'time_left' if days_left >= 3 else 'time_left_warn'
+        msg += '{{l10n:token/%s:%d:%02d:%02d}}\n' % (token_name, days_left, hours_left, mins_left)
+        msg += '{{l10n:token/cnt:%d}}' % tdata['cnt']
+    else:
+        type = SystemMessages.SM_TYPE.Error
+        msg += '{{l10n:token/unknown_status}}\n%s' % utils.hide_guid(simplejson.dumps(tdata))
+    msg += '</textformat>'
+
+    if _tdataPrev is None or _tdataPrev['status'] != 'active' or tdata is None or tdata['status'] != 'active':
+        SystemMessages.pushMessage(msg, type)
+
+    if tdata is not None:
+        _tdataPrev = tdata
+        if not 'token' in tdata and tdataActive is not None:
+            tdata['token'] = tdataActive['token']
+        userprefs.set('tokens.{0}'.format(playerId), tdata)
+        userprefs.set('tokens.lastPlayerId', playerId)
+
+        networkServicesSettings = _makeNetworkServicesSettings(tdata)
+
+    global _token
+    _token = '' if tdata is None else tdata.get('token', '').encode('ascii')
+
+    return
+
+def _checkToken(playerId, token):
+    data = None
+    errStr = None
+    try:
+        req = "checkToken/%d" % playerId
+        if token is not None:
+            req += "/%s" % token.encode('ascii')
+        server = XVM_SERVERS[randint(0, len(XVM_SERVERS) - 1)]
+        (response, duration, errStr) = loadUrl(server, req)
+
+        #response= """{"status":"inactive"}"""
+        #response = """{"expires_at":1394834790589,"cnt":0,"_id":4630209,"status":"active","token":"84a45576-5f06-4945-a607-bbee61b4876a","__v":0,"start_at":1393625190589}"""
+        #response = """{"expires_at":1394817931657,"cnt":3,"_id":2178413,"status":"badToken","start_at":1393608331657}"""
+
+        if not response:
+            #err('Empty response or parsing error')
+            pass
+        else:
+            try:
+                if response is None:
+                    return None
+                response = response.strip()
+                data = None if response in ('', '[]') else simplejson.loads(response)
+                log(utils.hide_guid(response))
+            except Exception, ex:
+                errStr = 'Bad answer: ' + response
+                err('  ' + errStr)
+                data = None
+    except Exception, ex:
+        errStr = str(ex)
+        err(traceback.format_exc())
+
+    return (data, errStr)
+
+def _getXvmMessageHeader():
+    msg = '<textformat tabstops="[130]"><img src="img://../mods/shared_resources/xvm/res/icons/xvm/16x16t.png" vspace="-5">'
+    msg += '&nbsp;<a href="#XVM_SITE#"><font color="#E2D2A2">www.modxvm.com</font></a>\n\n'
+    rev = ''
+    try:
+        from __version__ import __revision__
+        rev = __revision__
+    except Exception, ex:
+        err(traceback.format_exc())
+    msg += '{{l10n:ver/currentVersion:%s:%s}}\n' % (config.config['xvmVersion'], rev)
+    msg += _getVersionText(config.config['xvmVersion']) + '\n'
+    if g_websock.enabled and g_websock.connected:
+        msg += '{{l10n:websock/not_connected}}\n'
+        if g_websock.last_error:
+            msg += '<font size="12">%s</font>\n' % str(g_websock.last_error)
+        msg += '\n'
+    return msg
+
+def _getVersionText(curVer):
+    msg = ''
+    global _verInfo
+    if _verInfo is not None:
+        if gameRegion in _verInfo:
+            data = _verInfo[gameRegion]
+            if utils.compareVersions(data['ver'], curVer) == 1:
+                return '{{l10n:ver/newVersion:%s:%s}}\n' % (data['ver'], data['message'])
+    return ''
+
+def _processClansInfo(data):
+    clans = data.get('persistClans', {})
+    clans.update(data.get('topClans', {}))
+    # DEBUG
+    #clans['JKHU'] = {'rank':100,'cid':1,'emblem':'http://stat.modxvm.com/emblems/top/{size}/61318.png'}
+    #clans['MWJL'] = {'rank':101,'cid':2,'emblem':'http://stat.modxvm.com/emblems/top/{size}/61318.png'}
+    #clans['GPTX'] = {'rank':0,'cid':3,'emblem':'http://stat.modxvm.com/emblems/top/{size}/61318.png'}
+    #clans['CJBZ'] = {'rank':1,'cid':4,'emblem':'http://stat.modxvm.com/emblems/top/{size}/61318.png'}
+    # /DEBUG
+    return clans
