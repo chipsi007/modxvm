d6fb11bc0cc5dd30fea9faacad3cae05b6671729
Mikhail Paulyshka <me@mixaill.tk>
2015-08-18 21:27:01 +0300

    [BUILD] fix calc_hash_for_xvm_integrity() for whitespaces in path

---
+++ src/xpm/xvm_hotfix/__init__.py
@@ -0,0 +1,39 @@
+""" XVM (c) www.modxvm.com 2013-2015 """
+
+#####################################################################
+# MOD INFO
+
+XFW_MOD_INFO = {
+    # mandatory
+    'VERSION':       '3.1.0',
+    'URL':           'http://www.modxvm.com/',
+    'UPDATE_URL':    'http://www.modxvm.com/en/download-xvm/',
+    'GAME_VERSIONS': ['0.9.9','0.9.10'],
+    # optional
+}
+
+#####################################################################
+
+import BigWorld
+from xfw import *
+from xvm_main.python.logger import *
+
+
+# TODO: remove after swf preloading refactoring
+def hide_exception(base, self, *args):
+    try:
+        base(self, *args)
+    except Exception, ex:
+        log('[XVM][hide_exception]: %s throwed exception: %s' % (base.__name__, ex.message))
+
+
+# Delayed registration
+def _RegisterEvents():
+    from gui.Scaleform.daapi.view.meta.WaitingViewMeta import WaitingViewMeta
+    OverrideMethod(WaitingViewMeta, 'showS', hide_exception)
+    OverrideMethod(WaitingViewMeta, 'hideS', hide_exception)
+
+    from gui.Scaleform.framework.entities.abstract.ContainerManagerMeta import ContainerManagerMeta
+    OverrideMethod(ContainerManagerMeta, 'as_showS', hide_exception)
+
+#BigWorld.callback(0, _RegisterEvents)
---
+++ src/xpm/xvm_ping/__init__.py
@@ -0,0 +1,111 @@
+""" XVM (c) www.modxvm.com 2013-2015 """
+
+#####################################################################
+# MOD INFO
+
+XFW_MOD_INFO = {
+    # mandatory
+    'VERSION':       '3.1.0',
+    'URL':           'http://www.modxvm.com/',
+    'UPDATE_URL':    'http://www.modxvm.com/en/download-xvm/',
+    'GAME_VERSIONS': ['0.9.9','0.9.10'],
+    # optional
+}
+
+#####################################################################
+# constants
+
+class XVM_PING_COMMAND(object):
+    PING = "xvm_ping.ping"
+    AS_PINGDATA = "xvm_ping.as.pingdata"
+    GETCURRENTSERVER = "xvm_ping.getcurrentserver"
+    AS_CURRENTSERVER = "xvm_ping.as.currentserver"
+
+#####################################################################
+# includes
+
+import traceback
+
+import BigWorld
+
+import simplejson
+from xfw import *
+from xvm_main.python.logger import *
+
+import pinger
+# import pinger_wg as pinger
+#import pinger_wg_new as pinger
+
+#####################################################################
+# event handlers
+
+# INIT
+
+def start():
+    from gui.shared import g_eventBus
+    g_eventBus.addListener(XFWCOMMAND.XFW_CMD, onXfwCommand)
+
+def fini():
+    from gui.shared import g_eventBus
+    g_eventBus.removeListener(XFWCOMMAND.XFW_CMD, onXfwCommand)
+
+# onXfwCommand
+
+_LOG_COMMANDS = (
+    #XVM_PING_COMMAND.PING,
+)
+
+# returns: (result, status)
+def onXfwCommand(cmd, *args):
+    try:
+        if IS_DEVELOPMENT and cmd in _LOG_COMMANDS:
+            debug("cmd=" + str(cmd) + " args=" + simplejson.dumps(args))
+        if cmd == XVM_PING_COMMAND.PING:
+            pinger.ping()
+            return (None, True)
+        if cmd == XVM_PING_COMMAND.GETCURRENTSERVER:
+            getCurrentServer()
+            return (None, True)
+    except Exception, ex:
+        err(traceback.format_exc())
+        return (None, True)
+    return (None, False)
+
+def getCurrentServer(*args, **kwargs):
+    from ConnectionManager import connectionManager
+    as_xfw_cmd(XVM_PING_COMMAND.AS_CURRENTSERVER, connectionManager.serverUserName if len(connectionManager.serverUserName) < 13 else connectionManager.serverUserNameShort)
+
+# WGPinger (WARNING: bugs with the multiple hosts)
+
+def PreDefinedHostList_autoLoginQuery(base, callback):
+    # debug('> PreDefinedHostList_autoLoginQuery')
+    import pinger_wg
+    if pinger_wg.request_sent:
+        BigWorld.callback(0, lambda: PreDefinedHostList_autoLoginQuery(base, callback))
+    else:
+        # debug('login ping: start')
+        pinger_wg.request_sent = True
+        BigWorld.WGPinger.setOnPingCallback(PreDefinedHostList_onPingPerformed)
+        base(callback)
+
+def PreDefinedHostList_onPingPerformed(result):
+    # debug('login ping: end')
+    pinger_wg.request_sent = False
+    from predefined_hosts import g_preDefinedHosts
+    g_preDefinedHosts._PreDefinedHostList__onPingPerformed(result)
+
+# Delayed registration
+def _RegisterEvents():
+    start()
+
+    import game
+    RegisterEvent(game, 'fini', fini)
+
+    from gui.Scaleform.daapi.view.meta.LobbyHeaderMeta import LobbyHeaderMeta
+    RegisterEvent(LobbyHeaderMeta, 'as_setServerS', getCurrentServer)
+
+    # enable for pinger_wg
+    # from predefined_hosts import g_preDefinedHosts
+    # OverrideMethod(g_preDefinedHosts, 'autoLoginQuery', PreDefinedHostList_autoLoginQuery)
+
+BigWorld.callback(0, _RegisterEvents)
