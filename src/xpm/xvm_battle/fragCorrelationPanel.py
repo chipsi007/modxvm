a1ba8eddf02d02ba32c03c46c90eaa1aeb6a2874
Yaroslav Brustinov <monk143@bk.ru>
2015-08-23 01:58:07 +0300

    xvm_tooltips: fix

---
+++ src/xpm/xvm_battle/fragCorrelationPanel.py
@@ -0,0 +1,67 @@
+""" XVM (c) www.modxvm.com 2013-2015 """
+
+#####################################################################
+# imports
+
+import traceback
+
+import BigWorld
+from gui.shared.gui_items.Vehicle import VEHICLE_BATTLE_TYPES_ORDER_INDICES
+import gui.Scaleform.daapi.view.battle.score_panel as score_panel
+from gui.battle_control import g_sessionProvider
+
+from xfw import *
+
+from xvm_main.python.logger import *
+import xvm_main.python.config as config
+
+
+#####################################################################
+# handlers
+
+# show quantity of alive instead of dead in frags panel
+# original idea/code by yaotzinv: http://forum.worldoftanks.ru/index.php?/topic/1339762-
+@overrideMethod(score_panel._FragCorrelationPanel, 'updateScore')
+def FragCorrelationPanel_updateScore(base, self):
+    try:
+        if config.get('fragCorrelation/showAliveNotFrags'):
+            if len(self._FragCorrelationPanel__teamsDeaths) and len(self._FragCorrelationPanel__teamsShortLists):
+                isTeamEnemy = g_sessionProvider.getArenaDP().isEnemyTeam
+                ally_frags, enemy_frags, ally_vehicles, enemy_vehicles  = (0, 0, 0, 0)
+                for teamIdx, vehs in self._FragCorrelationPanel__teamsShortLists.iteritems():
+                    if isTeamEnemy(teamIdx):
+                        enemy_vehicles += len(vehs)
+                    else:
+                        ally_vehicles += len(vehs)
+                for teamIdx, score in self._FragCorrelationPanel__teamsDeaths.iteritems():
+                    if isTeamEnemy(teamIdx):
+                        ally_frags += score
+                    else:
+                        enemy_frags += score
+            team_left = ally_vehicles - enemy_frags
+            enemy_left = enemy_vehicles - ally_frags
+            self._FragCorrelationPanel__callFlash('updateFrags', [team_left, enemy_left])
+            return
+    except Exception, ex:
+        err(traceback.format_exc())
+    base(self)
+
+
+@overrideMethod(score_panel, '_markerComparator')
+def _markerComparator(base, x1, x2):
+    try:
+        if config.get('fragCorrelation/sortByType'):
+            INDEX_IS_ALIVE = 2
+            INDEX_VEHICLE_CLASS = 1
+            res = x2[INDEX_IS_ALIVE] - x1[INDEX_IS_ALIVE]
+            if res:
+                return res
+            x1Index = VEHICLE_BATTLE_TYPES_ORDER_INDICES.get(x1[INDEX_VEHICLE_CLASS], 100)
+            x2Index = VEHICLE_BATTLE_TYPES_ORDER_INDICES.get(x2[INDEX_VEHICLE_CLASS], 100)
+            res = x1Index - x2Index
+            if res:
+                return res
+            return 0
+    except Exception, ex:
+        err(traceback.format_exc())
+    return base(x1, x2)
---
+++ src/xpm/xvm_squad/__init__.py
@@ -0,0 +1,106 @@
+""" XVM (c) www.modxvm.com 2013-2015 """
+
+#####################################################################
+# MOD INFO
+
+XFW_MOD_INFO = {
+    # mandatory
+    'VERSION':       '3.1.0',
+    'URL':           'http://www.modxvm.com/',
+    'UPDATE_URL':    'http://www.modxvm.com/en/download-xvm/',
+    'GAME_VERSIONS': ['0.9.9','0.9.10'],
+    # optional
+}
+
+
+#####################################################################
+# imports
+
+import BigWorld
+import game
+from gui.shared import g_eventBus, g_itemsCache
+from gui.Scaleform.daapi.view.lobby.prb_windows.SquadView import SquadView
+
+from xfw import *
+
+import xvm_main.python.config as config
+from xvm_main.python.logger import *
+from xvm_main.python.xvm import l10n
+from xvm_main.python.vehinfo_tiers import getTiers
+
+
+#####################################################################
+# constants/globals
+
+class COMMANDS(object):
+    AS_UPDATE_TIERS = 'xvm_squad.as_update_tiers'
+    WINDOW_POPULATED = 'xvm_squad.window_populated'
+    WINDOW_DISPOSED = 'xvm_squad.window_disposed'
+
+window_populated = False
+squad_window_handler = None
+
+
+#####################################################################
+# initialization/finalization
+
+def start():
+    g_eventBus.addListener(XFWCOMMAND.XFW_CMD, onXfwCommand)
+
+BigWorld.callback(0, start)
+
+
+@registerEvent(game, 'fini')
+def fini():
+    g_eventBus.removeListener(XFWCOMMAND.XFW_CMD, onXfwCommand)
+
+
+#####################################################################
+# onXfwCommand
+
+# returns: (result, status)
+def onXfwCommand(cmd, *args):
+    global window_populated
+    if cmd == COMMANDS.WINDOW_POPULATED:
+        window_populated = True
+        squad_update_tiers(squad_window_handler) # squad_window_handler should be set by now
+        return (None, True)
+    if cmd == COMMANDS.WINDOW_DISPOSED:
+        window_populated = False
+        return (None, True)
+    return (None, False)
+
+
+#####################################################################
+# handlers
+
+@registerEvent(SquadView, '__init__')
+def SquadView__init__(self, *args, **kwargs):
+    squad_update_tiers(self, *args, **kwargs)
+
+
+@registerEvent(SquadView, '_updateRallyData')
+def SquadView_updateRallyData(self, *args, **kwargs):
+    squad_update_tiers(self, *args, **kwargs)
+
+
+def squad_update_tiers(self, *args, **kwargs):
+    try:
+        global squad_window_handler
+        squad_window_handler = self
+        if not window_populated:
+            return
+        min_tier = 0
+        max_tier = 0
+        for squad_vehicle in self.unitFunctional.getUnit()[1].getVehicles().values():
+            veh = g_itemsCache.items.getItemByCD(squad_vehicle['vehTypeCompDescr'])
+            (veh_tier_low, veh_tier_high) = getTiers(veh.level, veh.type, veh.name)
+            min_tier = max(veh_tier_low, min_tier)
+            max_tier = max(veh_tier_high, max_tier)
+    
+        text_tiers = ''
+        if min_tier > 0:
+            text_tiers = ' - %s: %s..%s' % (l10n('Squad battle tiers'), min_tier, max_tier)
+        as_xfw_cmd(COMMANDS.AS_UPDATE_TIERS, text_tiers)
+    except Exception, ex:
+        err(traceback.format_exc())
