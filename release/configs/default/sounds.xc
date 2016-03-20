fb446f4e53bd359f931f0fd79cea967d5088097d
night_dragon_on <night_dragon_on@localhost>
2016-03-20 16:40:10 +0500

    todo: add "enemySighted" event

---
+++ release/configs/default/sounds.xc
@@ -0,0 +1,63 @@
+﻿/**
+ * Extra sounds settings.
+ * Настройки дополнительных звуков.
+ * http://www.koreanrandom.com/forum/topic/18955-
+ */
+{
+  "sounds": {
+    // List of extra banks to load from folder res_mods/X.Y.Z/audioww.
+    // Use semicolon for multiple values: "bank1.bnk; bank2.bnk;..."
+    // Список дополнительных банков для загрузки из папки res_mods/X.Y.Z/audioww.
+    // Несколько банков необходимо указывать через точку с запятой: "bank1.bnk; bank2.bnk;..."
+    "soundBanks": {
+      "hangar": "xvm.bnk;",
+      "battle": "xvm.bnk;"
+    },
+    // Enable sound events logging in the xvm.log
+    // Включить логгирование звуковых событий в xvm.log
+    "logSoundEvents": false,
+    // Sound events remapping
+    // Переопределение звуковых событий
+    "soundMapping": {
+      // Event mapping
+      // Переопределение события
+      //"originalEventName": "newEventName"
+      // To disable sound event use empty string for value
+      // Для отключения звукового события используйте пустую строку для значения
+      //"originalEventName": ""
+      //
+      // Disable original sixth sense light bulb sound event
+      // Отключить оригинальный звук лампы шестого чувства
+      "lightbulb": "",
+      // Disable original fire sound event
+      // Отключить оригинальный звук пожара
+      //"vo_fire_started": "",
+      // Disable original ammo bay damaged event
+      // Отключить оригинальный звук повреждения боеукладки
+      //"vo_ammo_bay_damaged": "",
+      //
+      // Disable original enemy detection event
+      // Отключить оригинальный звук обнаружения противника
+      //"enemy_sighted_for_team": "",
+      //
+      // Sound events added by XVM
+      // Звуковые события, добавленные в XVM
+      //
+      // Perk SixthSense
+      // Перк Шестое чувство
+      "xvm_sixthSense": "sixthsense",
+      // Perk SixthSense (On vehicle Т-34-85 Rudy)
+      // Перк Шестое чувство (На технике Т-34-85 Rudy)
+      "xvm_sixthSenseRudy": "sixthSenseRudy",
+      // Fire alert
+      // Пожар
+      "xvm_fireAlert": "fireAlert",
+      // Damage ammoBay
+      // Повреждение боеукладки
+      "xvm_ammoBay": "ammoBay",
+      // Enemy detection (Use in together with disable original enemy detection event) 
+      // Обнаружения противника (Используйте вместе о отключением оригинального звук обнаружения противника)
+      "xvm_enemySighted": ""
+    }
+  }
+}
---
+++ src/xpm/xvm_sounds/__init__.py
@@ -0,0 +1,130 @@
+""" XVM (c) www.modxvm.com 2013-2015 """
+
+#####################################################################
+# MOD INFO
+
+XFW_MOD_INFO = {
+    # mandatory
+    'VERSION':       '0.9.14',
+    'URL':           'http://www.modxvm.com/',
+    'UPDATE_URL':    'http://www.modxvm.com/en/download-xvm/',
+    'GAME_VERSIONS': ['0.9.14'],
+    # optional
+}
+
+
+#####################################################################
+# imports
+
+import traceback
+
+import BigWorld
+import SoundGroups
+import WWISE
+from gui.Scaleform.Battle import Battle
+from gui.Scaleform.daapi.view.battle.damage_panel import DamagePanel
+from gui.Scaleform.Minimap import Minimap
+from gui.battle_control import g_sessionProvider
+
+from xfw import *
+
+import xvm_main.python.config as config
+from xvm_main.python.logger import *
+
+#####################################################################
+# constants
+
+class XVM_SOUND_EVENT(object):
+    SIXTH_SENSE = "xvm_sixthSense"
+    SIXTH_SENSE_RUDY = "xvm_sixthSenseRudy"
+    FIRE_ALERT = "xvm_fireAlert"
+    AMMO_BAY = "xvm_ammoBay"
+    ENEMY_SIGHTED = "xvm_enemySighted"
+
+#####################################################################
+# handlers
+
+@overrideMethod(WWISE, 'WG_loadBanks')
+def WWISE_WG_loadBanks(base, *args, **kwargs):
+    extraBanks = config.get('sounds/soundBanks/%s' % ('hangar' if args[1] else 'battle'))
+    if extraBanks:
+        lst = list(args)
+        lst[0] += extraBanks
+        args = tuple(lst)
+    log('WWISE.WG_loadBanks: %s' % args[0])
+    base(*args, **kwargs)
+
+
+@overrideMethod(SoundGroups.g_instance, 'checkAndReplace')
+def SoundGroups_g_instance_checkAndReplace(base, event):
+    event = base(event)
+    if not event:
+        return event
+    mappedEvent = config.get('sounds/soundMapping/%s' % event)
+    logSoundEvents = config.get('sounds/logSoundEvents')
+    if mappedEvent is not None:
+        if mappedEvent == '':
+            mappedEvent = 'emptyEvent'
+        if logSoundEvents:
+            log('SOUND EVENT: %s => %s' % (event, mappedEvent))
+        return mappedEvent
+    else:
+        if logSoundEvents:
+            log('SOUND EVENT: %s' % event)
+        return event
+
+
+# new sound events dispatchers
+
+@registerEvent(Battle, '_showSixthSenseIndicator')
+def Battle_showSixthSenseIndicator(self, isShow):
+    try:
+        vehId = BigWorld.entities[BigWorld.player().playerVehicleID].typeDescriptor.type.compactDescr
+        # 59393 => Rudy
+        soundId = XVM_SOUND_EVENT.SIXTH_SENSE_RUDY if vehId == 59393 else XVM_SOUND_EVENT.SIXTH_SENSE
+        SoundGroups.g_instance.playSound2D(soundId)
+    except:
+        err(traceback.format_exc())
+
+
+@registerEvent(Battle, '_setFireInVehicle')
+def Battle_setFireInVehicle(self, bool):
+    try:
+        SoundGroups.g_instance.playSound2D(XVM_SOUND_EVENT.FIRE_ALERT)
+    except:
+        err(traceback.format_exc())
+
+
+@registerEvent(DamagePanel, '_updateDeviceState')
+def DamagePanel_updateDeviceState(self, value):
+    try:
+        module, state, _ = value
+        if module == 'ammoBay' and state == 'critical':
+            SoundGroups.g_instance.playSound2D(XVM_SOUND_EVENT.AMMO_BAY)
+    except:
+        err(traceback.format_exc())
+
+
+enemyList = {}
+
+@overrideMethod(Minimap, '_Minimap__addEntry')
+def Minimap_Minimap__addEntry(base, self, vInfo, guiProps, location, doMark):
+    try:
+        if vInfo.vehicleID not in enemyList and not guiProps.isFriend:
+            enemyList[vInfo.vehicleID] = True
+            if doMark and not g_sessionProvider.getCtx().isPlayerObserver():
+                SoundGroups.g_instance.playSound2D(XVM_SOUND_EVENT.ENEMY_SIGHTED)
+    except:
+        err(traceback.format_exc())
+
+    return base(self, vInfo, guiProps, location, doMark)
+
+
+# TEST
+
+def _test():
+    log('test')
+    SoundGroups.g_instance.playSound2D(XVM_SOUND_EVENT.SIXTH_SENSE_RUDY)
+    BigWorld.callback(3, _test)
+
+#BigWorld.callback(10, _test)
