6f9f7440b4e1db766c1b411d3bdd17c1805a6cc9
demon2597 <demon2597@yandex.ru>
2016-03-22 00:19:01 +0300

    update XVMSymbol.ttf (change artillery symbol)

---
+++ src/xpm/xvm_hotfix/__init__.py
@@ -0,0 +1,30 @@
+""" XVM (c) www.modxvm.com 2013-2015 """
+
+#####################################################################
+# MOD INFO
+
+XFW_MOD_INFO = {
+    # mandatory
+    'VERSION':       '0.9.14',
+    'URL':           'http://www.modxvm.com/',
+    'UPDATE_URL':    'http://www.modxvm.com/en/download-xvm/',
+    'GAME_VERSIONS': ['0.9.14'],
+    # optional
+}
+
+
+#####################################################################
+#imports
+
+import BigWorld
+from gui.Scaleform.framework.entities.abstract.ContainerManagerMeta import ContainerManagerMeta
+
+from xfw import *
+
+from xvm_main.python.logger import *
+
+
+#####################################################################
+
+import update_arenas_data
+update_arenas_data.run()
