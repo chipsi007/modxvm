5ee4aa7d39715765507a141f6c9f2589cd698213
Roman Astafev <astafev.roman.mail@gmail.com>
2016-04-07 01:58:44 +0000

    fix: BattleMain.as

---
+++ build/bin/java/deswf.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ffdec="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/ffdec.sh"
+cfg=autoDeobfuscate=0,parallelSpeedUp=0
+fmt=script:as
+exp=script
+#exp=all
+
+for i in *.swf; do
+  if [ "$i" != "*.swf" ]; then
+    echo $i
+    $ffdec -config $cfg -format $fmt -export $exp ${i/.swf/} $i >$i.log 2>$i.err
+  fi
+done
