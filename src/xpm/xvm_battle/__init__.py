2a4358347402ce4c499cd3d132c6d9f23554c51f
sirmax2 <max@modxvm.com>
2017-01-18 22:16:18 +0200

    logs

---
+++ src/xpm/xvm_battle/__init__.py
@@ -0,0 +1,24 @@
+""" XVM (c) www.modxvm.com 2013-2017 """
+
+#####################################################################
+# MOD INFO
+
+XFW_MOD_INFO = {
+    # mandatory
+    'VERSION':       '0.9.17.0.2',
+    'URL':           'http://www.modxvm.com/',
+    'UPDATE_URL':    'http://www.modxvm.com/en/download-xvm/',
+    'GAME_VERSIONS': ['0.9.17.0.2'],
+    # optional
+}
+
+#####################################################################
+# imports
+
+import battle
+import battleloading
+import fragCorrelationPanel
+import minimap
+import camera
+import vehicleMarkers
+import vehicleMarkersBC
---
+++ src/xvm/xvm_battle/com/xvm/battle/BattleXvmApp.as
@@ -0,0 +1,23 @@
+package com.xvm.battle
+{
+    import com.xfw.*;
+    import com.xvm.*;
+
+    public class BattleXvmApp extends XvmAppBase
+    {
+        private var battleXvmMod:BattleXvmMod;
+
+        public function BattleXvmApp():void
+        {
+            Logger.counterPrefix = "B";
+
+            battleXvmMod = new BattleXvmMod();
+            addChild(battleXvmMod);
+
+            BattleGlobalData.init();
+            Xmqp.init();
+            Stat.clearBattleStat();
+            Stat.loadBattleStat();
+        }
+    }
+}
